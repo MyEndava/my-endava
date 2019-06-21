d63fa791c0c1a8c2a31c6dcebf74f3e4bff0f1b2
Mihai Rosu <mihai.rosu@endava.com>
2019-06-22 01:10:01 +0300

    added tag description to users activity

--- app/src/main/java/com/endava/myendava/UsersActivity.java
+++ app/src/main/java/com/endava/myendava/UsersActivity.java
@@ -2,18 +2,21 @@ package com.endava.myendava;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.endava.myendava.activities.ProfileActivity;
+import com.endava.myendava.rest.RetrofitClient;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.endava.myendava.activities.ProfileActivity;
-import com.endava.myendava.rest.RetrofitClient;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
@@ -21,6 +24,11 @@ import io.reactivex.schedulers.Schedulers;
 public class UsersActivity extends AppCompatActivity implements UsersAdapter.OnUserClickListener {
 
     public static final String ARG_TAG = "arg_tag_name";
+
+    @BindView(R.id.tag_description_text_view)
+    TextView descriptionTextView;
+    @BindView(R.id.collapsing_toolbar_layout)
+    CollapsingToolbarLayout collapsingToolbarLayout;
 
     private Tag tag;
     private List<User> users = new ArrayList<>();
@@ -31,6 +39,7 @@ public class UsersActivity extends AppCompatActivity implements UsersAdapter.OnU
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
+        ButterKnife.bind(this);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -39,6 +48,9 @@ public class UsersActivity extends AppCompatActivity implements UsersAdapter.OnU
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle(tag.getTagName());
         }
+
+        descriptionTextView.setText(tag.getTagDescription());
+        collapsingToolbarLayout.post(() -> collapsingToolbarLayout.setScrimVisibleHeightTrigger(collapsingToolbarLayout.getMeasuredHeight() - 150));
 
         RecyclerView recyclerView = findViewById(R.id.people_recycler_view);
         recyclerView.setHasFixedSize(true);
