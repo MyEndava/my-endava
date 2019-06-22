c49db0315777a0df4043e6f7ccbf7e051bab90ef
Alex Graur <grauralex@gmail.com>
2019-06-22 22:22:37 +0300

    fixed profile bug

--- app/src/main/java/com/endava/myendava/activities/ProfileActivity.java
+++ app/src/main/java/com/endava/myendava/activities/ProfileActivity.java
@@ -5,13 +5,22 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.endava.myendava.ProfileFragment;
 import com.endava.myendava.R;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    public static final String ARG_EMAIL = "arg_email";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        if (savedInstanceState == null) {
+            String email = getIntent().getStringExtra(ARG_EMAIL);
+            getSupportFragmentManager().beginTransaction().add(R.id.profile_container,
+                    ProfileFragment.newInstance(email)).commit();
+        }
     }
 }
