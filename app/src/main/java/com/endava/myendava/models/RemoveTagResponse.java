ffafb3d451783853597ac8092639dd891d62258b
soundrob <paltanrobert@yahoo.com>
2019-10-15 13:22:30 +0300

    First commit Remove Tags from profile

---
+++ app/src/main/java/com/endava/myendava/models/RemoveTagResponse.java
@@ -0,0 +1,11 @@
+package com.endava.myendava.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class RemoveTagResponse {
+
+    @SerializedName("success")
+    @Expose
+    public boolean success;
+}
